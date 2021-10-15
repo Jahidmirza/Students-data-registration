d66e76308f2cfb8645d6ecff5a89cd3f7598a831
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-15 21:04:39 +0600

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+ // web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyAsb6OHViGLriIjlTyZHTlPoI6-5zuWk4U",
+    authDomain: "students-data-c1887.firebaseapp.com",
+    projectId: "students-data-c1887",
+    storageBucket: "students-data-c1887.appspot.com",
+    messagingSenderId: "332687302158",
+    appId: "1:332687302158:web:1d3e1318d2e92b72a7665f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
