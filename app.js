460bcf1da2c3e306cf9092c2c6ac12b6c6f458fc
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-15 22:30:37 +0600

    done

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
- // web app's Firebase configuration
- const firebaseConfig = {
-    apiKey: "AIzaSyAsb6OHViGLriIjlTyZHTlPoI6-5zuWk4U",
-    authDomain: "students-data-c1887.firebaseapp.com",
-    projectId: "students-data-c1887",
-    storageBucket: "students-data-c1887.appspot.com",
-    messagingSenderId: "332687302158",
-    appId: "1:332687302158:web:1d3e1318d2e92b72a7665f"
+const config = {
+    apiKey: "AIzaSyCEsIqdOo7-BdvufQkb6OZj3XlRW7d6hFk",
+    authDomain: "students-data-e7bd0.firebaseapp.com",
+    projectId: "students-data-e7bd0",
+    storageBucket: "students-data-e7bd0.appspot.com",
+    messagingSenderId: "921524719350",
+    appId: "1:921524719350:web:dac5e15174976b7fb326d7"
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
-  
+  const messagesRef = firebase.database().ref('messages');
+
+
+document.querySelector('.dataForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    
+const name = getInputVal('name');
+const email = getInputVal('email');
+const roll = getInputVal('roll');
+const section = getInputVal('section');
+const dateBrt = getInputVal('dateBrt');
+const mobile = getInputVal('mobile');
+const address = getInputVal('address');
+const city = getInputVal('city');
+const male = getInputVal('male');
+const female = getInputVal('female');
+
+
+    console.log(name);
+}
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
